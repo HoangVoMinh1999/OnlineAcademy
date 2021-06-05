cf35585cc17f4d7937aac5304d1d011f5879a081
Duy Ha <hacaoduy161@gmail.com>
2021-06-05 22:28:42 +0700

    add: register, login

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { singleByUserName } = require('../models/user.model');
 const userModel = require('../models/user.model');
 
 const router = express.Router();
@@ -8,18 +10,18 @@ router.get('/', async function (req, res, next) {
     res.json(listteacher);
 })
 
-router.post('/', async function (req, res, next) {
-    constuser = req.body;
-    if (teacher.name !== undefined) {
-        user.Log_CreatedDate = new Date();
-        const ids = await userModel.addteacher(teacher);
-        user.id = ids[0];
-        res.status(200).json(teacher);
-    }
-    res.json({
-        'err_message': 'No content for user'
-    });
-})
+// router.post('/', async function (req, res, next) {
+//     const user = req.body;
+//     if (teacher.name !== undefined) {
+//         user.Log_CreatedDate = new Date();
+//         const ids = await userModel.addteacher(teacher);
+//         user.id = ids[0];
+//         res.status(200).json(teacher);
+//     }
+//     res.json({
+//         'err_message': 'No content for user'
+//     });
+// })
 
 router.patch('/delete/:id', async function (req, res, next) {
     const id = req.params.id;
@@ -54,5 +56,48 @@ router.patch('/:id', async function (req, res, next) {
     res.json({
         'message': 'Update successfully !!!'
     })
+})
+
+router.post('/register', async function (req, res, next) {
+    const user = req.body;
+    if (user.username === undefined || user.password === undefined) {
+        return res.status(400).json({
+            'message': 'Invalid username or password!'
+        })
+    }
+    const tmp = await userModel.singleByUserName(user.username);
+    if (tmp !== null) {
+        return res.status(400).json({
+            'message': 'Username is available!'
+        })
+    }
+    user.password = bcrypt.hashSync(user.password, 10);
+    user.Log_CreatedDate = new Date();
+    const ids = await userModel.addteacher(user);
+    user.id = ids[0];
+    return res.status(200).json({
+        'message': 'Register successfully!'
+    });
+
+})
+
+router.post('/login', async function (req, res, next) {
+    const user = req.body;
+    const tmp = await userModel.singleByUserName(user.username);
+    if (tmp === null) {
+        return res.status(404).json({
+            'message': 'Username is not exist!'
+        })
+    }
+    const isMatch = bcrypt.compareSync(user.password, tmp.password);
+    if (isMatch === false)
+    {
+        return res.status(400).json({
+            'message': 'Password is incorrect!'
+        })
+    }
+    return res.status(200).json({
+        'message': 'Login successfully!'
+    });
 })
 module.exports = router;
