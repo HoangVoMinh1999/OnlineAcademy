0b3878e387ff6bbd45117ee57f7a6e75232a4267
hacaoduy161@gmail.com <hacaoduy161@gmail.com>
2021-08-19 18:08:57 +0700

    add: disable account

--- Frontend/admin/src/Components/User/UserList.jsx
+++ Frontend/admin/src/Components/User/UserList.jsx
@@ -31,7 +31,7 @@ class UserList extends Component {
                 switch (value) {
                     case "confirm":
                         const res = await userService.updateUser(id, { IsTeacher: true });
-                        if (!res.err_message){
+                        if (!res.err_message) {
                             swal('Nâng cấp thành công !!!');
                             const res = await userService.getAllUser();
                             this.props.dispatch(
@@ -59,7 +59,63 @@ class UserList extends Component {
                 switch (value) {
                     case "confirm":
                         const res = await userService.updateUser(id, { IsTeacher: false });
-                        if (!res.err_message){
+                        if (!res.err_message) {
+                            swal('Giáng cấp thành công !!!');
+                            const res = await userService.getAllUser();
+                            this.props.dispatch(
+                                createAction(
+                                    GET_USER_LIST,
+                                    res.data,
+                                )
+                            )
+                        }
+                }
+            });
+    }
+
+    handleDisableUser = (id) => {
+        swal("Bạn có chắc chắn muốn khóa tài khoản này không ???", {
+            buttons: {
+                confirm: {
+                    text: "Xác nhận",
+                    value: "confirm",
+                },
+                cancel: true,
+            },
+        })
+            .then(async (value) => {
+                switch (value) {
+                    case "confirm":
+                        const res = await userService.updateUser(id, { IsDisabled: true });
+                        if (!res.err_message) {
+                            swal('Giáng cấp thành công !!!');
+                            const res = await userService.getAllUser();
+                            this.props.dispatch(
+                                createAction(
+                                    GET_USER_LIST,
+                                    res.data,
+                                )
+                            )
+                        }
+                }
+            });
+    }
+
+    handleAbleUser = (id) => {
+        swal("Bạn có chắc chắn muốn mở khóa tài khoản này không ???", {
+            buttons: {
+                confirm: {
+                    text: "Xác nhận",
+                    value: "confirm",
+                },
+                cancel: true,
+            },
+        })
+            .then(async (value) => {
+                switch (value) {
+                    case "confirm":
+                        const res = await userService.updateUser(id, { IsDisabled: false });
+                        if (!res.err_message) {
                             swal('Giáng cấp thành công !!!');
                             const res = await userService.getAllUser();
                             this.props.dispatch(
@@ -76,13 +132,29 @@ class UserList extends Component {
     renderContent = () => {
         if (this.props.userList !== undefined && this.props.userList !== null && this.props.userList.length > 0) {
             return this.props.userList.map((item, index) => {
+                let role = "";
+                if (item.IsAdmin.data[0] === 1){
+                    role = "Admin";
+                }
+                else if (item.IsTeacher.data[0] === 1){
+                    role = "Giảng viên";
+                }
+                else{
+                    role = "Học sinh";
+                }
                 return <tr key={index}>
                     <td>{item.id}</td>
                     <td>{item.name === null ? 'Chưa có thông tin' : item.name}{item.IsAdmin.data[0] === 1 ? <i className="fa fa-star"></i> : ''}</td>
                     <td>{item.address === null ? 'Chưa có thông tin' : item.address}</td>
                     <td>{item.phone === null ? 'Chưa có thông tin' : item.phone}</td>
                     <td>{item.email === null ? 'Chưa có thông tin' : item.email}</td>
-                    <td>{item.IsTeacher.data[0] === 0 ? 'Học sinh' : 'Giảng viên'}</td>
+                    <td>{role}</td>
+                    <td>
+                        {item.IsDisabled.data[0] === 0 ?
+                            <button type="button" class="btn btn-success" hidden={item.IsAdmin.data[0] === 1} onClick={() => this.handleAbleUser(item.id)}>Mở tài khoản </button> :
+                            <button type="button" class="btn btn-danger" hidden={item.IsAdmin.data[0] === 1} onClick={() => this.handleDisableUser(item.id)}>Khóa tài khoản</button>
+                        }
+                    </td>
                     <td>
                         {item.IsTeacher.data[0] === 0 ?
                             <button type="button" class="btn btn-success" hidden={item.IsAdmin.data[0] === 1} onClick={() => this.handleUpgradeUser(item.id)}>Nâng cấp tài khoản</button> :
@@ -119,6 +191,7 @@ class UserList extends Component {
                                                 <th>Số điện thoại</th>
                                                 <th>Email</th>
                                                 <th>Chức vụ</th>
+                                                <th>Khóa tài khoản</th>
                                                 <th>Khác</th>
                                             </tr>
                                         </thead>
